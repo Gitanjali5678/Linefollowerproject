4d4c56c8a86ee7915796170725e4c595aa078e52
Gitanjali5678 <146728905+Gitanjali5678@users.noreply.github.com>
2023-10-02 17:54:14 +0530

    Add files via upload

---
+++ Arduino_linefollowerCode.ino
@@ -0,0 +1,46 @@
+//line follow bot
+
+void setup() {
+ pinMode(2,INPUT);
+ pinMode(3,INPUT);
+ pinMode(4,OUTPUT);
+ pinMode(5,OUTPUT);
+ pinMode(6,OUTPUT);
+ pinMode(7,OUTPUT);
+for(int i=4;i<=7;i++) {
+  pinMode(i,OUTPUT);
+}
+}
+
+void loop() {
+ int ir1 = digitalRead(2);
+ int ir2 = digitalRead(3);
+
+if(ir1==1 && ir2==1) {
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+}
+
+if(ir1==0 && ir2==1) {
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+}
+
+if(ir1==1 && ir2==0) {
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+}
+
+if(ir1==0 && ir2==0) {
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+}
+}
